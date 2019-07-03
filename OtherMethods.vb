4b7c7eb2eb2be7150c41c8460d208c6a03ed5d1f
quoctrong1x96 <quoctrong1x96@gmail.com>
2019-07-03 17:38:31 +0700

    Create OtherMethods.vb

---
+++ OtherMethods.vb
@@ -0,0 +1,24 @@
+Friend Module OtherMethods
+
+    Private PF As PopupForm
+
+    Public Sub ExitApplication()
+        'Perform any clean-up here
+        'Then exit the application
+        Application.Exit()
+    End Sub
+
+    Public Sub ShowDialog()
+        If PF IsNot Nothing AndAlso Not PF.IsDisposed Then Exit Sub
+
+        Dim CloseApp As Boolean = False
+
+        PF = New PopupForm
+        PF.ShowDialog()
+        CloseApp = (PF.DialogResult = DialogResult.Abort)
+        PF = Nothing
+
+        If CloseApp Then Application.Exit()
+    End Sub
+
+End Module
